221ae7f3147a3d14043524fb4fa28d289d5d5b95
Miguel Garcia Alcala <mgarcia2@c3r4s4.42malaga.com>
2026-03-02 15:39:24 +0100

    subida

---
+++ CURSUS/python_discovery/module10/ex0/lambda_spells.py
@@ -0,0 +1,11 @@
+def artifact_sorter(artifacts: list[dict]) -> list[dict]:
+    return sorted(artifacts, key=lambda artifact: artifact["power"], reverse=True)
+
+def power_filter(mages: list[dict], min_power: int) -> list[dict]
+    return
+
+def spell_transformer(spells: list[str]) -> list[str]
+    return
+
+def mage_stats(mages: list[dict]) -> dict
+    return
